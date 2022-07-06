dc485be7531eb84fae4d7039ad84370d393113aa
GusarovArtem <artemmaksimovich1@gmail.com>
2022-07-06 07:43:31 +0300

    Add Webpack

--- src/main/java/ua/jupiter/controller/MainController.java
+++ src/main/java/ua/jupiter/controller/MainController.java
@@ -1,6 +1,7 @@
 package ua.jupiter.controller;
 
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,19 +14,27 @@ import java.util.HashMap;
 
 @Controller
 @RequestMapping("/")
-@RequiredArgsConstructor
 public class MainController {
+    private final MessageRepository messageRepo;
 
-    private final MessageRepository messageRepository;
+    @Value("${spring.profiles.active}")
+    private String profile;
+
+    @Autowired
+    public MainController(MessageRepository messageRepo) {
+        this.messageRepo = messageRepo;
+    }
 
     @GetMapping
     public String main(Model model, @AuthenticationPrincipal User user) {
         HashMap<Object, Object> data = new HashMap<>();
 
         data.put("profile", user);
-        data.put("messages", messageRepository.findAll());
+        data.put("messages", messageRepo.findAll());
 
         model.addAttribute("frontendData", data);
+        model.addAttribute("isDevMode", "dev".equals(profile));
+
         return "index";
     }
 }
