0343bfc3f193a6bf562943700136f0fc84b87df1
GusarovArtem <artemmaksimovich1@gmail.com>
2022-07-07 09:20:41 +0300

    Add vuex for storing pages

--- src/main/resources/static/js/pages/App.vue
+++ src/main/resources/static/js/pages/App.vue
@@ -3,7 +3,7 @@
     <v-toolbar app>
       <v-toolbar-title>Jupiter</v-toolbar-title>
       <v-spacer></v-spacer>
-      <span v-if="profile">{{ profile.name }}</span>
+      <span v-if="profile">{{profile.name}}</span>
       <v-btn v-if="profile" icon href="/logout">
         <v-icon>exit_to_app</v-icon>
       </v-btn>
@@ -14,48 +14,41 @@
         <a href="/login">Google</a>
       </v-container>
       <v-container v-if="profile">
-        <messages-list :messages="messages"/>
+        <messages-list />
       </v-container>
     </v-content>
   </v-app>
 </template>
 
 <script>
+import { mapState, mapMutations } from 'vuex'
 import MessagesList from 'components/messages/MessageList.vue'
-import {addHandler} from 'util/ws'
+import { addHandler } from 'util/ws'
 
 export default {
   components: {
     MessagesList
   },
-  data() {
-    return {
-      messages: frontendData.messages,
-      profile: frontendData.profile
-    }
-  },
+  computed: mapState(['profile']),
+  methods: mapMutations(['addMessageMutation', 'updateMessageMutation', 'removeMessageMutation']),
   created() {
     addHandler(data => {
       if (data.objectType === 'MESSAGE') {
-        const index = this.messages.findIndex(item => item === data.body.id)
-
         switch (data.eventType) {
           case 'CREATE':
+            this.addMessageMutation(data.body)
+            break
           case 'UPDATE':
-            if (index > -1) {
-              this.messages.splice(index, 1, data.body)
-            } else {
-              this.messages.push(data.body)
-            }
+            this.updateMessageMutation(data.body)
             break
           case 'REMOVE':
-            this.messages.splice(index, 1)
+            this.removeMessageMutation(data.body)
             break
           default:
-            console.error('Looks like the event type is unknown ${data.eventType}')
+            console.error(`Looks like the event type if unknown "${data.eventType}"`)
         }
       } else {
-        console.error('Looks like the object type is unknown ${data.objectType}')
+        console.error(`Looks like the object type if unknown "${data.objectType}"`)
       }
     })
   }
